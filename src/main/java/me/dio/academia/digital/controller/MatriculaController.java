09fc828ad217f893da52cda2c4efa96c58cc6b4a
Joel Henrique <joelhenrique78@gmail.com>
2023-04-13 18:19:32 -0300

    att2 13/04/2023

--- src/main/java/me/dio/academia/digital/controller/MatriculaController.java
+++ src/main/java/me/dio/academia/digital/controller/MatriculaController.java
@@ -1,9 +1,17 @@
 package me.dio.academia.digital.controller;
 
+import me.dio.academia.digital.entity.Matricula;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/matriculas")
 public class MatriculaController {
+
+    public List<Matricula> getAll(@RequestParam(value = "bairro", required = false) String bairro) {
+        return service;
+    }
 }
--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,7 +1,11 @@
 package me.dio.academia.digital.entity.form;
 
-public class MatriculaForm {
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
+public class MatriculaForm {
+  @NotNull(message = "Preencha o campo corretamente")
+  @Positive(message = "O id do aluno precisa ser positivo.")
   private Long alunoId;
 
 }
