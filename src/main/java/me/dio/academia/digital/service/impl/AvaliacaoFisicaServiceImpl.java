2ceff0cf07432639bc0ee6fd1c346aa140697dd3
Joel Henrique <joelhenrique78@gmail.com>
2023-04-13 17:02:15 -0300

    att 13-04-2023

--- src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
@@ -1,2 +1,56 @@
-package me.dio.academia.digital.service.impl;public class AvaliacaoFisicaServiceImpl {
+package me.dio.academia.digital.service.impl;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
+import me.dio.academia.digital.service.IAvaliacaoFisicaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AvaliacaoFisicaServiceImpl implements IAvaliacaoFisicaService {
+
+    @Autowired
+    private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+        AvaliacaoFisica avaliacao = new AvaliacaoFisica();
+
+        Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+
+        avaliacao.setAluno(aluno);
+        avaliacao.setPeso(form.getPeso());
+        avaliacao.setAltura(form.getAltura());
+
+        return avaliacaoFisicaRepository.save(avaliacao);
+    }
+
+    @Override
+    public AvaliacaoFisica get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<AvaliacaoFisica> getAll() {
+        return avaliacaoFisicaRepository.findAll();
+    }
+
+    @Override
+    public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+    }
+
 }
